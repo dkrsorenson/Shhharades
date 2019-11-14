d9be61846ed6245e226c78259b8583a4593b352c
lgp71 <duosport77@yahoo.com>
2019-11-13 21:33:06 -0500

    main menu added

---
+++ Shhharades/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] Transform canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ToGameWon()
+    {
+        SceneManager.LoadScene("GameWon");
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("Start");
+        Time.timeScale = 1f;
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+    
+    public void ResumeGame()
+    {
+        GameObject.Find("PauseMenu").SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void GoToInstructions()
+    {
+        gameObject.SetActive(false);
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+        }
+    }
+
+    public void BackToPause()
+    {
+        gameObject.SetActive(false);
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+        }
+    }
+}
